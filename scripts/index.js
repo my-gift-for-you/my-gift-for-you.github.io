bd07115211cb00444b44247abdb719b198b60109
geavenx <vitordonnangeloc@gmail.com>
2023-05-27 14:41:07 -0300

    day counter added

--- scripts/index.js
+++ scripts/index.js
@@ -25,3 +25,25 @@ document
       document.getElementById("submit-button").click();
     }
   });
+
+function countDays() {
+  const startDate = new Date("2022-05-29");
+  let nowDate = new Date();
+
+  let diff = nowDate.getTime() - startDate.getTime();
+  let days = Math.floor(diff / (1000 * 60 * 60 * 24));
+  let hours = Math.floor(diff / (1000 * 60 * 60));
+  let minutes = Math.floor(diff / (1000 * 60));
+  let seconds = Math.floor(diff / 1000);
+  document.getElementById("counter").innerHTML =
+    "Passaram-se <span class='dias'>" +
+    days +
+    " dias</span>, <span class='hours'>" +
+    hours +
+    " horas</span>, <span class='minutes'>" +
+    minutes +
+    " minutos</span>, <span class='seconds'>" +
+    seconds +
+    " segundos</span> desde o ínicio dos meus dias mais felizes <3";
+}
+setInterval(countDays, 1000);
